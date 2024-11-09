598cf1064afb29ca3d895af41c225164be3d170d
Biker7395 <94505416+SethProYT@users.noreply.github.com>
2024-11-09 03:40:56 +0000

    vghjghjgjh

--- themind.h
+++ themind.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>
+#include <time.h>
 
 #define MAX_LENGTH 1000
 
@@ -13,4 +14,52 @@ void capitalizer(char* dest, const char* src) {
         dest[i] = toupper(src[i]);
     }
     dest[i] = '\0';
+}
+
+void rnpns(char *mv) {
+    srand(time(NULL));
+    int r = rand() % 3;
+    int player;
+    // rock is one
+    // paper is two
+    // scissors is three
+
+    printf("rock or paper or scissors: r/p/s\n");
+    char ans[100];
+    char mod[100];
+    fgets(ans, 100, stdin);
+    ans[strcspn(ans, "\n")] = 0;
+
+    capitalizer(mod, ans);
+
+    switch(ans[0]) {
+        case 'R':
+            if (r == 3) {
+                printf("dang you won gg\n");
+            } 
+            else if (r == 2) {
+                printf("you lost haha now rm -rf time\n");
+            }
+            else if (r == 1) {
+                printf("it's a tie");
+            }
+            break;
+        case 'P':
+            if (r == 3) {
+                printf("i cut your paper, i win.\n");
+            }
+            else if (r == 1) {
+                printf("noo my rock!!");
+            }
+            else if (r == 2) {
+                printf("it's a tie.");
+            }
+            break;
+        case 'S':
+            // Handle scissors case
+            break;
+        default:
+            printf("Invalid input\n");
+            break;
+    }
 }
